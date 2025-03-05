25fea82c889f5ca97c6c36fdc07a44c8bf80fbba
Arnattachai <behind321@gmail.com>
2025-03-05 17:00:14 +0700

    Bug fix#2 -adding student id validation (id must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,34 +1,36 @@
 function checkSID() {
-  let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function checkCandiNo() {
-  let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function validateForm(){
-	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
-	  document.getElementById("sid").focus();
+	let sid = (document.getElementById("sid").value).trim();
+	if (sid.length == 10) {
+	if (sid.length == 10 && /^[0-9]{10}$/.test(sid))  {
+	  return true;
+	} else {
 	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
-			alert("Your input data passes validation!!");
-			return true;
-		}
 	}
-}
+  }
+  
+  function checkCandiNo() {
+	let candi = (document.getElementById("candi").value).trim();
+	if (isNaN(candi) || candi < 1) {
+	if (isNaN(candi) || candi < 1 || candi > 10) {
+	  return false;
+	} else {
+	  return true;
+	}
+  }
+  
+  function validateForm(){
+	  if(!checkSID()){
+		alert("Invalid value for Student ID!");
+		document.getElementById("sid").focus();
+		return false;
+	  }else{
+		  if(!checkCandiNo()){
+			alert("Invalid value for Candidate No!!");
+			document.getElementById("candi").focus();
+			return false;
+		  }else{
+			  alert("Your input data passes validation!!");
+			  return true;
+		  }
+	  }
+  }
